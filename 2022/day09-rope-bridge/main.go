3f96cce00e4316f9a0ac6524cb65cea1c07dd049
papayankey <yankeybenneth@gmail.com>
2022-12-28 00:50:58 +0000

    refactor(day09) part1 of rope bridge

--- 2022/day09-rope-bridge/main.go
+++ 2022/day09-rope-bridge/main.go
@@ -41,8 +41,8 @@ func PartTwo(moves [][]string) int {
 				colDifference := head[1] - tail[1]
 				notTouchingKnots := utils.Abs(rowDifference) > 1 || utils.Abs(colDifference) > 1
 				if notTouchingKnots {
-					tail[0] += sign(rowDifference)
-					tail[1] += sign(colDifference)
+					tail[0] += utils.Sign(rowDifference)
+					tail[1] += utils.Sign(colDifference)
 					if j == len(knots)-1 {
 						TailVisits[fmt.Sprintf("(%v,%v)", tail[0], tail[1])] = true
 					}
@@ -54,50 +54,28 @@ func PartTwo(moves [][]string) int {
 	return len(TailVisits)
 }
 
-func sign(n int) int {
-	if n == 0 {
-		return 0
-	}
-	if n < 0 {
-		return -1
-	}
-	return 1
-}
-
 func PartOne(moves [][]string) int {
-	Head := []int{5, 1}
-	Tail := []int{5, 1}
+	head := []int{5, 1}
+	tail := []int{5, 1}
 
-	TailVisits := map[string]int{"(5,1)": 1}
+	TailVisits := map[string]bool{"(5,1)": true}
 
 	for _, move := range moves {
 		direction, amount := move[0], utils.Atoi(move[1])
 		for i := 1; i <= amount; i++ {
-			Head[0], Head[1] = directions(direction)[0]+Head[0], directions(direction)[1]+Head[1]
-			if utils.Abs(Head[0]-Tail[0]) > 1 || utils.Abs(Head[1]-Tail[1]) > 1 {
-				if Head[0] != Tail[0] && Head[1] != Tail[1] {
-					if Head[1] > Tail[1] {
-						if Head[0] < Tail[0] {
-							// move top-right
-							Tail[0], Tail[1] = Tail[0]+directions("UR")[0], Tail[1]+directions("UR")[1]
-						} else {
-							// move bottom-right
-							Tail[0], Tail[1] = Tail[0]+directions("DR")[0], Tail[1]+directions("DR")[1]
-						}
-					} else {
-						if Head[0] < Tail[0] {
-							// move top-left
-							Tail[0], Tail[1] = Tail[0]+directions("UL")[0], Tail[1]+directions("UL")[1]
-						} else {
-							// move bottom-left
-							Tail[0], Tail[1] = Tail[0]+directions("DL")[0], Tail[1]+directions("DL")[1]
-						}
-					}
-				} else {
-					// move behind head either same row or column
-					Tail[0], Tail[1] = directions(direction)[0]+Tail[0], directions(direction)[1]+Tail[1]
-				}
-				TailVisits[fmt.Sprintf("(%v,%v)", Tail[0], Tail[1])] += 1
+			// move head knot
+			head[0] += directions(direction)[0]
+			head[1] += directions(direction)[1]
+
+			rowDifference := head[0] - tail[0]
+			colDifference := head[1] - tail[1]
+			notTouchingKnots := utils.Abs(rowDifference) > 1 || utils.Abs(colDifference) > 1
+
+			// move tail knot if distance between cols or rows is greater than 1
+			if notTouchingKnots {
+				tail[0] += utils.Sign(rowDifference)
+				tail[1] += utils.Sign(colDifference)
+				TailVisits[fmt.Sprintf("(%v,%v)", tail[0], tail[1])] = true
 			}
 		}
 	}
@@ -105,13 +83,11 @@ func PartOne(moves [][]string) int {
 }
 
 func directions(name string) []int {
-	directions := map[string][]int{
+	dirs := map[string][]int{
 		"R": {0, 1}, "L": {0, -1},
 		"U": {-1, 0}, "D": {1, 0},
-		"UR": {-1, 1}, "UL": {-1, -1},
-		"DR": {1, 1}, "DL": {1, -1},
 	}
-	return directions[name]
+	return dirs[name]
 }
 
 func readLines(name string) [][]string {
