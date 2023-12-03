c6d3c38f9736cfa53f4f472070dd4d04b30d89b6
papayankey <yankeybenneth@gmail.com>
2023-12-03 00:33:25 +0000

    add day 2 solution

---
+++ 2023/aoc/src/main/java/com/github/papayankey/day_02/CubeConundrum.java
@@ -0,0 +1,88 @@
+package com.github.papayankey.day_02;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+import com.github.papayankey.InputSource;
+
+public class CubeConundrum {
+    public static void main(String[] args) {
+        var input = InputSource.load("day_02");
+
+        // Part One
+        var result = partOne(input);
+        System.out.println(result);
+
+        // Part Two
+        result = partTwo(input);
+        System.out.println(result);
+    }
+
+    private static int partOne(Path inputSource) {
+        var maximumCubes = Map.of("red", 12, "green", 13, "blue", 14);
+        var result = 0;
+
+        try (var scanner = new Scanner(inputSource)) {
+            while (scanner.hasNextLine()) {
+                var parts = scanner.nextLine().trim().split(":");
+                var gameId = Integer.parseInt(parts[0].trim().split(" ")[1]);
+                var cubes = parts[1].trim().replaceAll("; |, ", ",").split(",");
+                var isValidGame = true;
+
+                for (int i = 0; i < cubes.length; i++) {
+                    var cube = cubes[i].trim().split(" ");
+                    var cubeScore = Integer.parseInt(cube[0]);
+                    var cubeColor = cube[1].trim();
+
+                    if (cubeScore > maximumCubes.get(cubeColor)) {
+                        isValidGame = false;
+                    }
+                }
+
+                if (isValidGame) {
+                    result += gameId;
+                }
+            }
+        } catch (IOException exception) {
+            System.out.println(exception.getMessage());
+        }
+
+        return result;
+    }
+
+    private static int partTwo(Path inputSource) {
+        var result = 0;
+
+        try (var scanner = new Scanner(inputSource)) {
+            while (scanner.hasNextLine()) {
+                var minimumCubes = new HashMap<String, Integer>();
+                var parts = scanner.nextLine().trim().split(":");
+                var cubes = parts[1].trim().replaceAll("; |, ", ",").split(",");
+
+                for (int i = 0; i < cubes.length; i++) {
+                    var cube = cubes[i].trim().split(" ");
+                    var cubeScore = Integer.parseInt(cube[0]);
+                    var cubeColor = cube[1].trim();
+
+                    if (minimumCubes.containsKey(cubeColor)) {
+                        if (cubeScore > minimumCubes.get(cubeColor)) {
+                            minimumCubes.put(cubeColor, cubeScore);
+                        }
+                    } else {
+                        minimumCubes.put(cubeColor, cubeScore);
+                    }
+                }
+
+                var power = minimumCubes.values().stream().reduce(1, (accum, curr) -> accum * curr);
+                result += power;
+            }
+        } catch (IOException exception) {
+            System.out.println(exception.getMessage());
+        }
+
+        return result;
+    }
+}
