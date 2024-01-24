f92f6cc423ce5ea78330d177a87fd3eb66327261
papayankey <yankeybenneth@gmail.com>
2024-01-24 03:26:05 +0000

    Add 2023 day07 part2

--- src/main/java/com/github/papayankey/AoC_2023/day_07/CamelCards.java
+++ src/main/java/com/github/papayankey/AoC_2023/day_07/CamelCards.java
@@ -16,54 +16,69 @@ public class CamelCards {
         var input = AoC.getInput(2023, 7);
 
         System.out.println(PartOne(input));
+        System.out.println(PartTwo(input));
     }
 
     static long PartOne(List<String> lines) {
+        return calculateWinning(lines, false);
+    }
+
+    static long PartTwo(List<String> lines) {
+        return calculateWinning(lines, true);
+    }
+
+    static long calculateWinning(List<String> lines, boolean addJokers) {
         long[] rank = {1};
-        return lines.stream().map(CamelCards::getHand)
+        return lines.stream().map(line -> getHand(line, addJokers))
                 .sorted()
                 .mapToLong(hand -> hand.bid * rank[0]++)
                 .sum();
     }
 
-    static long PartTwo(List<String> lines) {
-        return 0L;
-    }
-
-    static Hand getHand(String str) {
+    static Hand getHand(String str, boolean addJokers) {
         List<String> pairs = Arrays.stream(str.split("\\s+")).toList();
-        List<Card> cards = Arrays.stream(pairs.getFirst().split("")).map(CamelCards::getCard).toList();
+        List<Card> cards = Arrays.stream(pairs.getFirst().split("")).map(s -> getCard(s, addJokers)).toList();
 
         List<Long> pattern = cards.stream().collect(groupingBy(Card::name, counting())).entrySet().stream()
                 .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
                 .map(Map.Entry::getValue)
                 .toList();
 
-        return new Hand(cards, getKind(pattern), Long.parseLong(pairs.getLast()));
+        return new Hand(cards, getKind(cards, pattern, addJokers), Long.parseLong(pairs.getLast()));
     }
 
-    static Kind getKind(List<Long> pattern) {
+    static Kind getKind(List<Card> cards, List<Long> pattern, boolean addJokers) {
+        var jokerCount = cards.stream().filter(card -> card.name == 'J').count();
         var size = pattern.size();
-        if (size == 1) return FIVE_OF_KIND;
-        if (size == 2) {
-            if (pattern.getFirst() == 4) return FOUR_OF_KIND;
-            return FULL_HOUSE;
-        }
-        if (size == 3) {
-            if (pattern.getFirst() == 3) return THREE_OF_KIND;
-            return TWO_PAIR;
-        }
-        if (size == 4) return ONE_PAIR;
-        return HIGH_CARD;
+
+        return switch (size) {
+            case 1 -> FIVE_OF_KIND;
+            case 2 -> {
+                if (pattern.getFirst() == 4) {
+                    yield addJokers && (jokerCount == 1 || jokerCount == 4) ? FIVE_OF_KIND : FOUR_OF_KIND;
+                }
+                yield addJokers && (jokerCount == 2 || jokerCount == 3) ? FIVE_OF_KIND : FULL_HOUSE;
+            }
+            case 3 -> {
+                if (pattern.getFirst() == 3) {
+                    yield addJokers && (jokerCount == 1 || jokerCount == 3) ? FOUR_OF_KIND : THREE_OF_KIND;
+                }
+                yield addJokers && jokerCount == 1 ?
+                        FULL_HOUSE : addJokers && jokerCount == 2 ?
+                        FOUR_OF_KIND : TWO_PAIR;
+            }
+            case 4 -> addJokers && (jokerCount == 1 || jokerCount == 2) ? THREE_OF_KIND : ONE_PAIR;
+            default -> addJokers && jokerCount == 1 ? ONE_PAIR : HIGH_CARD;
+        };
     }
 
-    static Card getCard(String str) {
+    static Card getCard(String str, boolean addJokers) {
         char c = str.charAt(0);
         return switch (c) {
             case 'A' -> new Card(c, 14);
             case 'K' -> new Card(c, 13);
             case 'Q' -> new Card(c, 12);
-            case 'J' -> new Card(c, 11);
+            case 'J' -> addJokers ? new Card(c, 1) : new Card(c, 11);
             case 'T' -> new Card(c, 10);
             default -> new Card(c, c - '0');
         };
