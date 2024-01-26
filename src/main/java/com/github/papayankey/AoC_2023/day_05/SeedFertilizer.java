9355138487f1d06a9009030cf4934f3261689042
papayankey <yankeybenneth@gmail.com>
2024-01-26 09:30:05 +0000

    Add 2023 day05 part1

--- src/main/java/com/github/papayankey/AoC_2023/day_05/SeedFertilizer.java
+++ src/main/java/com/github/papayankey/AoC_2023/day_05/SeedFertilizer.java
@@ -1,89 +1,111 @@
 package com.github.papayankey.AoC_2023.day_05;
 
+import com.github.papayankey.AoC;
+
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
 public class SeedFertilizer {
     public static void main(String[] args) {
-//        var input = AoC.getInput(2023, 5);
+        var input = AoC.getInput(2023, 5);
+        input = Arrays.stream(String.join("\n", input).split("\n\n")).toList();
 
-        var input = """
-                seeds: 79 14 55 13
-                                
-                seed-to-soil map:
-                50 98 2
-                52 50 48
-                                
-                soil-to-fertilizer map:
-                0 15 37
-                37 52 2
-                39 0 15
-                                
-                fertilizer-to-water map:
-                49 53 8
-                0 11 42
-                42 0 7
-                57 7 4
-                                
-                water-to-light map:
-                88 18 7
-                18 25 70
-                                
-                light-to-temperature map:
-                45 77 23
-                81 45 19
-                68 64 13
-                                
-                temperature-to-humidity map:
-                0 69 1
-                1 0 69
-                                
-                humidity-to-location map:
-                60 56 37
-                56 93 4
-                """;
-
-        PartOne(input);
+        System.out.println(PartOne(input));
+        System.out.println(PartTwo(input));
     }
 
-    static void PartOne(String input) {
-        List<String> groups = Arrays.stream(input.split("\n\n")).toList();
+    static long PartOne(List<String> input) {
+        List<Long> seeds = getSeeds(input);
+        List<List<Category>> categories = getCategories(input);
 
-        List<Long> seeds = Arrays.stream(groups.getFirst().replaceAll("seeds:\\s+", "").split("\\s+"))
+        List<Long> locations = new ArrayList<>();
+
+        for (long value : seeds) {
+            for (List<Category> category : categories) {
+                value = nextMappedValue(value, category);
+            }
+            locations.add(value);
+        }
+
+        return locations.stream().min(Comparator.naturalOrder()).orElse(0L);
+    }
+
+    static long PartTwo(List<String> input) {
+        List<Range> seeds = getSeedsWithRange(getSeeds(input));
+        List<List<Category>> categories = getCategories(input);
+
+        List<Long> locations = new ArrayList<>();
+
+//        for (Range range : seeds) {
+//            for (long i = range.start; i <= range.end; i++) {
+//                long value = i;
+//                for (List<Category> category : categories) {
+//                    value = getNextCategoryValue(value, category);
+//                }
+//                locations.add(value);
+//            }
+//        }
+//
+//        System.out.println(STR."locations: \{locations}");
+//        return locations.stream().mapToLong(Long::longValue).min().orElse(0L);
+
+        return 0L;
+    }
+
+    static List<Range> getSeedsWithRange(List<Long> seeds) {
+        List<Range> ranges = new ArrayList<>();
+
+        int index = 0;
+        while (index < seeds.size()) {
+            int endIndex = index + 2; // exclusive
+            List<Long> pair = seeds.subList(index, endIndex);
+            ranges.add(new Range(pair.getFirst(), pair.getFirst() + pair.getLast() - 1));
+            index = endIndex;
+        }
+
+        return ranges;
+    }
+
+    static List<Long> getSeeds(List<String> input) {
+        return Arrays.stream(input.getFirst().replaceAll("seeds:\\s+", "").split("\\s+"))
                 .map(Long::parseLong)
                 .toList();
-
-        List<List<Resource>> resources = parseResources(groups.subList(1, groups.size()));
     }
 
-    static void PartTwo(List<String> lines) {
+    static long nextMappedValue(long value, List<Category> categories) {
+        for (Category category : categories) {
+            if (value >= category.src.start && value <= category.src.end) {
+                return category.dest.start + (value - category.src.start);
+            }
+        }
+        return value;
     }
 
-    static List<List<Resource>> parseResources(List<String> resourceGroups) {
-        List<List<String>> resources = resourceGroups.stream()
+    static List<List<Category>> getCategories(List<String> input) {
+        List<List<Category>> categories = new ArrayList<>();
+
+        List<List<String>> resources = input.stream()
+                .skip(1)
                 .map(line -> Arrays.stream(line.split("\n")).skip(1).toList())
                 .toList();
 
-        return List.of(
-                getResources(resources.getFirst()), getResources(resources.get(1)),
-                getResources(resources.get(2)), getResources(resources.get(3)),
-                getResources(resources.get(4)), getResources(resources.get(5)), getResources(resources.get(6))
-        );
-    }
-
-    static List<Resource> getResources(List<String> list) {
-        List<Resource> resources = new ArrayList<>();
-        for (var str : list) {
-            long[] parts = Arrays.stream(str.split("\\s+")).mapToLong(Long::parseLong).toArray();
-            Range destination = new Range(parts[0], parts[0] + parts[2] - 1);
-            Range source = new Range(parts[1], parts[1] + parts[2] - 1);
-            resources.add(new Resource(destination, source));
+        for (List<String> resource : resources) {
+            List<Category> temp = new ArrayList<>();
+            for (String str : resource) {
+                long[] parts = Arrays.stream(str.split("\\s+")).mapToLong(Long::parseLong).toArray();
+                Range destination = new Range(parts[0], parts[0] + parts[2] - 1);
+                Range source = new Range(parts[1], parts[1] + parts[2] - 1);
+                temp.add(new Category(destination, source));
+            }
+            categories.add(temp);
         }
-        return resources;
+
+        return categories;
     }
 
-    record Resource(Range dest, Range src) {
+    record Category(Range dest, Range src) {
     }
 
     record Range(long start, long end) {
