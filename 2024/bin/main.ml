2c80df7497c589391e9b60aae2b7dd5633f005b0
papayankey <yankeybenneth@gmail.com>
2024-12-03 02:22:06 +0000

    Update to receive advent year and day from command line

--- 2024/bin/main.ml
+++ 2024/bin/main.ml
@@ -2,6 +2,13 @@ open Aoc2024.Util
 
 let () =
   set_session ();
+  let year =
+    print_endline "Input advent year: ";
+    read_int ()
+  and day =
+    print_endline "Input advent day: ";
+    read_int ()
+  in
   let file_path = "data/prod.txt" in
-  Lwt_main.run (get_day_input 2024 1 file_path)
+  Lwt_main.run (get_day_input year day file_path)
 ;;
